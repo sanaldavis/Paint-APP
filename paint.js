5c89e02571354d86e86ee061a240b3a72f9ac483
sanaldavis <sanaldavis@yahoo.co.in>
2013-11-08 11:09:23 +0530

    Second stage of paint

--- paint.js
+++ paint.js
@@ -1,4 +1,34 @@
 var canvas= document.getElementById('canvas');
 var context= canvas.getContext('2d');
-context.strokeRect(10,10,100,100);
-context.fillRect(120,120,300,300);
+
+function make_Rect() {
+	context.strokeRect(10,10,100,100);
+	context.fillStyle= 'rgba(0,0,255,0.5)';
+	context.fillRect(120,120,300,300);
+}
+
+function make_Text() {
+	context.font= "24px Helvetica"
+	context.fillText("Click anywhere to erase",120,150);
+}
+
+function make_Line() {
+	context.lineWidth= 1;
+	context.beginPath();
+	context.moveTo(50,10);
+	context.lineTo(450,10);
+	context.stroke();
+	context.beginPath();
+	context.moveTo(50.50,50.50);
+	context.lineTo(450.50,50.50);
+	context.stroke();
+}
+
+make_Text();
+make_Rect();
+make_Line();
+
+context.canvas.onmousedown = function(e) {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
